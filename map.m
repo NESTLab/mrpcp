012175ecb1bb28a35507cb3c89570d0f41dbc99c
Dharini <dharinidutia@gmail.com>
2018-06-22 18:28:57 -0400

    digraph

--- map.m
+++ map.m
@@ -1,32 +1,32 @@
 %% Environment map
 
 % Targets
-xt = randi(10,targets,1);
-yt = randi(10,targets,1);
+xt = randi(25,targets,1);
+yt = randi(25,targets,1); 
+% xt = [5;7;8;2;7];
+% yt = [3;9;1;4;8];
 
 % Depots
-xd = randi(10,depots,1);
-yd = randi(10,depots,1);
+xd = randi(25,depots,1);
+yd = randi(25,depots,1);
+% xd = [6;9;5];
+% yd = [5;3;7];
 
-nodes = [xt,yt;xd,yd];
+x_pos = [xt',xd'];
+y_pos = [yt',yd'];
+nodes = [x_pos',y_pos'];
 
-% A = ones(total_nodes);
-% t = diag(ones(1,total_nodes));
-% A = A -t;
-% G = graph(A);
+A = zeros(total_nodes);
+G = digraph(A);
 
 %% Visualization
-% figure;
-% axis equal;
-% scatter(xt,yt);
-% xlim([0 10]);
-% ylim([0 10]);
-% hold on
-% scatter(xd,yd);
-% title("Map");
-% legend('Targets', 'Depots');
 
-%plot(G);
+figure;
+h = plot(G,'XData', x_pos, 'YData', y_pos);
+axis square
+hold on
+title("Map");
+highlight(h,[1:1:targets])
 
 %% Parameters
 
@@ -37,9 +37,15 @@ cij=zeros(total_nodes^2 *K,1);
 for k=1:K
     cij(1+(k-1)*total_nodes^2:(k*total_nodes^2),1)=cij_per_robot;
 end
+% e_dist = distances(G);
+% cij_per_robot = reshape(e_dist',total_nodes^2,1);
+% cij=zeros(total_nodes^2 *K,1);
+% for k=1:K
+%     cij(1+(k-1)*total_nodes^2:(k*total_nodes^2),1)=cij_per_robot;
+% end
 
 % Fuel cost between two nodes
-fij = e_dist;%ones(total_nodes);
+fij = ones(total_nodes);
 
 
 
